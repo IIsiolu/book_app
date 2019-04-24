7210d80b54999a74da7b078e4c4098ad37f33a4c
IIsiolu <david.isiolu@gmail.com>
2019-04-24 08:36:15 +0100

    my api
    complete api

---
+++ api/server/src/config/config.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+module.exports = {
+  // If using online database
+  // development: {
+  //  use_env_variable: 'DATABASE_URL'  
+  //},
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT
+  },
+
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT
+  },
+  
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT
+  },
+}
